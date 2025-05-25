dcdbd4687f517c02bce1739eb6fe7d8709eaecb0
Gerson Vieira <gersonbase@gmail.com>
2025-05-25 01:05:03 -0400

    add UserListViewModelProtocol

--- Snowpiercer/Domain/Protocols/UserListViewModelProtocol.swift
+++ Snowpiercer/Domain/Protocols/UserListViewModelProtocol.swift
@@ -5,6 +5,22 @@
 //  Created by Gerson Vieira on 25/05/25.
 //
 
-protocol UserListViewModelProtocol {
+import Foundation
+import Swiftagram
+
+protocol UserListViewModelProtocol: ObservableObject {
     
+    var followers: [InstagramUser] { get }
+    var following: [InstagramUser] { get }
+    var nonFollowers: [InstagramUser] { get }
+    
+    var searchText: String { get set }
+    var currentListType: UserSectionCard { get set }
+    var filteredUsers: [InstagramUser] { get }
+    var hasLoadedInitialData: Bool { get }
+    
+    func loadUserList(forceReload: Bool) async
+    func setup(with secret: Secret)
+    
+    func loadCachedLists()
 }
