21a8a2e9af03e7a576211e631bc82b5ec4669b85
Gerson Vieira <gersonbase@gmail.com>
2025-05-25 00:46:45 -0400

    add ParentDashboardViewModelProtocol

---
+++ Snowpiercer/Domain/Protocols/ParentDashboardViewModelProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  ParentDashboardViewModelProtocol.swift
+//  Snowpiercer
+//
+//  Created by Gerson Vieira on 24/05/25.
+//
+
+import Combine
+import Foundation
+
+protocol ParentDashboardViewModelProtocol: ErrorHandlingViewModel {
+    // MARK: - Estado da UI
+    var isLoading: Bool { get set }
+    var loadProgress: Double { get set }
+    
+    // MARK: - ViewModel filhos
+    var userDashboardViewModel: UserDashboardViewModelProtocol { get }
+    var userListViewModel: UserListViewModelProtocol { get }
+    
+    // MARK: - Carregar dados iniciais 
+    
+    func loadInitialData(account: SavedAccount) async
+    func refreshData() async
+}
